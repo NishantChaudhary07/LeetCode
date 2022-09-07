ccbbad17750de2cdb0760c5081cab7e751b911ca
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2022-09-08 00:28:50 +0530

    Time: 3 ms (99.32%), Space: 54.7 MB (80.46%) - LeetHub

---
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.java
@@ -0,0 +1,32 @@
+class Solution {
+    public static int fact(int n){
+		int fact=1;
+		for(int i=n;i>=1;i--){
+           fact*=i;
+		}
+		return fact;
+	}
+    public int subarraysDivByK(int[] arr, int k) {
+        int len=arr.length;
+			int[] rem=new int[k];
+			rem[0]=1;
+			int sum=0;
+			for(int i=0;i<len;i++){
+             sum+=arr[i];
+			 if(sum%k<0){
+				 rem[sum%k+k]++;
+			 }
+			 else{
+			 rem[sum%k]++;
+			 }
+			}
+			int count=0;
+			for(int i=0;i<k;i++){
+				
+                if(rem[i]>=2){
+					count=count+(rem[i]*(rem[i]-1))/2;
+				}
+			}
+        return count;
+    }
+}
