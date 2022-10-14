ad5b20b031461c47c9dd68b4cd72b20ddf15bfa1
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2022-10-14 21:57:31 +0530

    Time: 525 ms (12.92%), Space: 42.9 MB (61.43%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,4 +1,21 @@
 class Solution {
+    public static String expand(String s,int start,int end,String res){
+         
+           while(start>=0 && end<s.length()){
+               if(s.charAt(start)!=s.charAt(end))
+               {
+                  break; 
+               }
+               if(end!=start)
+                   res=s.charAt(start)+res+s.charAt(end);
+               else if(end==start)
+                   res=res+s.charAt(end);
+            
+                   start--;
+                   end++;
+           }
+        return res;
+    }
     public String longestPalindrome(String s) {
         int mxlen=0;
         String ans="";
@@ -6,40 +23,15 @@ class Solution {
             return s;
         }
        for(int i=0;i<s.length();i++){
-           String pal="";
-           int start=i,end=i;
-           while(start>=0 && end<s.length()){
-               if(s.charAt(start)!=s.charAt(end))
-               {
-                  break; 
-               }
-               if(end!=start)
-                   pal=s.charAt(start)+pal+s.charAt(end);
-               else if(end==start)
-                   pal=pal+s.charAt(end);
-               
-               if(end-start+1>mxlen){
-                   mxlen=end-start+1;
-                   ans=pal;
-               }
-                   start--;
-                   end++;
+          String res=expand(s,i,i,"");
+           String res1=expand(s,i,i+1,"");
+           if(res.length()>mxlen){
+               mxlen=res.length();
+               ans=res;
            }
-           
-            start=i;end=i+1;
-           String pal1="";
-           while(start>=0 && end<s.length()){
-               if(s.charAt(start)!=s.charAt(end))
-               {
-                  break; 
-               }
-               pal1=s.charAt(start)+pal1+s.charAt(end);
-               if(end-start+1>mxlen){
-                   mxlen=end-start+1;
-                   ans=pal1;
-               }
-                   start--;
-                   end++;
+           if(res1.length()>mxlen){
+               mxlen=res1.length();
+               ans=res1;
            }
        }
         return ans;
