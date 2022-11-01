9c2a3e1bd9a677132ebccf58f61ebea51cbc54f0
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2022-11-01 22:59:42 +0530

    Time: 5 ms (86.66%), Space: 43.9 MB (50.75%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public static void combination(int[] candidates,int target,List<Integer> list,List<List<Integer>> res,int idx){
+        if(target==0){
+            res.add(new ArrayList<Integer>(list));
+            return;
+        }
+        for(int i=idx;i<candidates.length;i++){
+            if(target>=candidates[i]){
+                list.add(candidates[i]);
+                combination(candidates,target-candidates[i],list,res,i+1);
+                list.remove(list.size()-1);
+                while(i<candidates.length-1 && candidates[i]==candidates[i+1]){
+                    i++;
+                }
+            }
+        }
+    }
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        Arrays.sort(candidates);
+        combination(candidates,target,list,res,0);
+        return res;
+    }
+}
