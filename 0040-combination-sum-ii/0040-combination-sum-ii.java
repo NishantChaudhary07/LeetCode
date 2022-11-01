ebfad7047553009e5668c6750a8b0e112474f768
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2022-11-01 23:47:19 +0530

    Time: 7 ms (56.61%), Space: 44.4 MB (17.33%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -5,13 +5,15 @@ class Solution {
             return;
         }
         for(int i=idx;i<candidates.length;i++){
+            if(i!=idx && candidates[i]==candidates[i-1])
+                continue;
             if(target>=candidates[i]){
                 list.add(candidates[i]);
                 combination(candidates,target-candidates[i],list,res,i+1);
                 list.remove(list.size()-1);
-                while(i<candidates.length-1 && candidates[i]==candidates[i+1]){
-                    i++;
-                }
+                // while(i<candidates.length-1 && candidates[i]==candidates[i+1]){
+                //     i++;
+                // }
             }
         }
     }
