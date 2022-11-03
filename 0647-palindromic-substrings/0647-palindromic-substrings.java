97116f64dd34e7bc71a043af44b55d386f488fd8
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2022-11-03 15:38:48 +0530

    Time: 5 ms (84.53%), Space: 40.5 MB (89.72%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,37 +1,22 @@
 class Solution {
-    public int countSubstrings(String str) {
+    public int countSubstrings(String s) {
         int count=0;
-		for(int i=0;i<str.length();i++){
-			 int right=i;
-		int left=i;
-		while(left>=0 && right<str.length()){
-            if(str.charAt(left)!=str.charAt(right)){
-				break;
-			}
-			if(str.charAt(left)==str.charAt(right)){
-				count+=1;
-			}
-			left--;
-			right++;
-		}
-            
-		}
-        
-        for(int i=1;i<str.length();i++){
-		    int right=i;
-		    int left=i-1;
-		while(left>=0 && right<str.length()){
-             if(str.charAt(left)!=str.charAt(right)){
-				break;
-			}
-			if(str.charAt(left)==str.charAt(right)){
-				count+=1;
-			}
-			left--;
-			right++;
-		}
-            
-		}
-		return count;
+      for(int axis=0;axis<s.length();axis++){
+          for(int orbit=0;orbit<s.length() && axis-orbit>=0 && axis+orbit<s.length();orbit++){
+              if(s.charAt(axis-orbit)!=s.charAt(axis+orbit)){
+                  break;
+              }
+              count++;
+          }
+      } 
+         for(double axis=0.5;axis<s.length();axis++){
+          for(double orbit=0.5;orbit<s.length() && axis-orbit>=0 && axis+orbit<s.length();orbit++){
+              if(s.charAt((int)(axis-orbit))!=s.charAt((int)(axis+orbit))){
+                  break;
+              }
+              count++;
+          }
+      }
+                 return count;
     }
 }
