09055f6a6a5df16866952405b06371eff8a56a54
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2022-11-11 22:33:16 +0530

    Time: 3 ms (77.25%), Space: 48 MB (56.25%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,21 +1,17 @@
 class Solution {
-    public static int no_of_1(int n){
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        if(n%2==0) 
-            return no_of_1(n/2);
-        else
-            return 1+no_of_1(n/2);
-    }
+
     public int[] countBits(int n) {
         int[] ans=new int[n+1];
-         for(int i=0;i<=n;i++){
-             ans[i]=no_of_1(i);
-         }
+        if(n==0)
+            return ans;
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+                ans[i]=ans[i/2];
+            }
+            else{
+                ans[i]=ans[i-1]+1;
+            }
+        }
         return ans;
     }
 }
