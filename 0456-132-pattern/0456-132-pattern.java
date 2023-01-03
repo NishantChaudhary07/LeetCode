2a2c50e9eb2fe6e28d33cc325d65efce6c5b7186
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-01-03 22:05:42 +0530

    Time: 51 ms (40.30%), Space: 67.9 MB (13.10%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -5,18 +5,18 @@ class Solution {
         for(int i=1;i<nums.length;i++){
             min[i]=Math.min(min[i-1],nums[i]);
         }
-        
         Stack<Integer> st=new Stack<>();
         for(int i=nums.length-1;i>=0;i--){
             int ith=min[i];
-            if(nums[i]>ith){
-                while(!st.isEmpty() && st.peek()<=ith){
+            if(ith<nums[i]){
+                while(!st.isEmpty() && nums[st.peek()]<=ith){
                     st.pop();
                 }
-                if(!st.isEmpty() && st.peek()<nums[i])
+                if(!st.isEmpty() && nums[i]>nums[st.peek()]){
                     return true;
+                }
             }
-            st.push(nums[i]);
+            st.push(i);
         }
         return false;
     }
