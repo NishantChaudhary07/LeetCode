2146e72a05cb8d2e6854dcd877eaf98e4ae8dca7
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-02-04 00:18:49 +0530

    Time: 1 ms (39.28%), Space: 42 MB (53.11%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,21 +13,33 @@
  *     }
  * }
  */
+class bstpair{
+    boolean isbst=true;
+    long max=Long.MIN_VALUE;
+    long min=Long.MAX_VALUE;
+}
 class Solution {
-   TreeNode prev=null;
+    private bstpair isvalidbst(TreeNode root){
+        if(root==null){
+            return new bstpair();
+        }
+        bstpair lbst=isvalidbst(root.left);
+        bstpair rbst=isvalidbst(root.right);
+        bstpair bst=new bstpair();
+        if(lbst.isbst==true && rbst.isbst==true && root.val>lbst.max && root.val<rbst.min){
+            bst.isbst=true;
+            bst.min=Math.min(lbst.min,root.val);
+            bst.max=Math.max(rbst.max,root.val);
+            return bst;
+        }
+        
+        bst.isbst=false;
+        bst.min=Math.min(lbst.min,Math.min(rbst.min,root.val));
+        bst.max=Math.max(lbst.max,Math.max(rbst.max,root.val));
+        return bst;
+    }
     public boolean isValidBST(TreeNode root) {
-        if(root==null)
-            return true;
-        if(!isValidBST(root.left))
-            return false;
-        
-        if(prev!=null && prev.val>=root.val)
-            return false;
-        prev=root;
-        
-        if(!isValidBST(root.right))
-            return false;
-        
-        return true;
+        // System.out.print(isvalidbst(root).min);
+        return isvalidbst(root).isbst;
     }
 }
