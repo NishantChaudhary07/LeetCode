91df951eff53d7f99d05622954700ac86e120408
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-02-08 15:14:31 +0530

    Time: 3 ms (40.77%), Space: 42.8 MB (71.52%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int far=0,end=0;
+        for(int i=0;i<nums.length-1;i++){
+            far=Math.max(far,i+nums[i]);
+            if(i==end){
+                if(end==far){
+                    return false;
+                }
+                end=far;
+            }
+        }
+        if(far>=nums.length-1){
+            return true;
+        }
+        return false;
+    }
+}
