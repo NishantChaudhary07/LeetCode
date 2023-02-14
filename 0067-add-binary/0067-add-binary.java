305d10e2cedee730688cb1238b2958504d8175cd
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-02-14 18:33:03 +0530

    Time: 7 ms (19.54%), Space: 42.1 MB (53.88%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,42 +1,29 @@
 class Solution {
     public String addBinary(String a, String b) {
-        int lena=a.length();
-        int lenb=b.length();
+        int lena=a.length()-1;
+        int lenb=b.length()-1;
        
         String res="";
         int carry=0;
-        while(true){
-            int nb=b.charAt(lenb-1)-'0';
-            int na=a.charAt(lena-1)-'0';
-            int sum=(nb+na+carry)%2;
-            carry=(nb+na+carry)/2;
-            res=sum+res;
-            lena--;
-            lenb--;
-             if(lena==0 || lenb==0){
-                break;
+        while(lena>=0 || lenb>=0){
+            int sum=0;
+            if(lena>=0){
+                int na=a.charAt(lena)-'0';
+                sum+=na;
+                lena--;
             }
-        }
-        if(lena!=0){
-             lena--;  
-        while(lena>=0){
-            int na=a.charAt(lena)-'0';
-            int sum=(na+carry)%2;
-            carry=(na+carry)/2;
-            res=sum+res;
-            lena--;
-        }
-        }
-        if(lenb!=0){
-             lenb--;
-            while(lenb>=0){
+            if(lenb>=0){
                 int nb=b.charAt(lenb)-'0';
-            int sum=(nb+carry)%2;
-            carry=(nb+carry)/2;
-            res=sum+res;
-            lenb--;
-        }
-        }
+                sum+=nb;
+                lenb--;
+            }
+            
+            int sum1=(sum+carry)%2;
+            carry=(sum+carry)/2;
+            // System.out.println(sum+","+carry);
+            res=sum1+res;
+            }
+      
         if(carry==1)
             res=carry+res;
         return res;
