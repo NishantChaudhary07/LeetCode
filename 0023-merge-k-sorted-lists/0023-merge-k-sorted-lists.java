a95e48587760271cdb21b4184bef548cdd97f90b
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-02-22 22:46:02 +0530

    Time: 239 ms (10.53%), Space: 321.3 MB (5.42%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -14,25 +14,21 @@ class Solution {
         ListNode head=list3;
         while(list1!=null && list2!=null){
             if(list1.val<list2.val){
-                list3.next=list1;
+                list3.next=new ListNode(list1.val);
                 list1=list1.next;
             }
             else{
-                list3.next=list2;
+                list3.next=new ListNode(list2.val);
                 list2=list2.next;
                 
             }
             list3=list3.next;
         }
-            while(list1!=null){
+            if(list1!=null){
                 list3.next=list1;
-                list1=list1.next;
-                list3=list3.next;
             }
-            while(list2!=null){
+            if(list2!=null){
                 list3.next=list2;
-                list2=list2.next;
-                list3=list3.next;
             } 
         return head.next;
     }
