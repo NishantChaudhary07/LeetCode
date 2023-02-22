26da88a26f790d29c81e5b0673918873a1325d23
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-02-23 00:13:15 +0530

    Time: 19 ms (39.97%), Space: 44.6 MB (13.62%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int countPaths(TreeNode root,int targetSum){
+        if(root==null){
+            return 0;
+        }
+        int ans=0;
+        if(targetSum-root.val==0){
+            ans++;
+        }
+        ans=ans+countPaths(root.left,targetSum-root.val);
+        ans=ans+countPaths(root.right,targetSum-root.val);
+        return ans;
+    }
+    public int pathSum(TreeNode root, int targetSum) {
+        if(root==null){
+            return 0;
+        }
+        if(root.val==1000000000){
+            return 0;
+        }
+        int root_sum=countPaths(root,targetSum);
+        int left=pathSum(root.left,targetSum);
+        int right=pathSum(root.right,targetSum);
+        return root_sum+left+right;
+    }
+}
