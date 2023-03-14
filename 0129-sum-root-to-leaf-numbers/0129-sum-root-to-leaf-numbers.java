95bbfa2304cbe3538714b65865197933681b79ac
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-03-14 12:54:58 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (24.48%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -13,19 +13,20 @@
  *     }
  * }
  */
+
 class Solution {
-    private int sum(TreeNode root,int num){
+    private int pathSum(TreeNode root,int num){
         if(root==null){
             return 0;
         }
+        num=num*10+root.val;
         if(root.left==null && root.right==null){
-            num=num*10+root.val;
             return num;
         }
-        return sum(root.left,num*10+root.val)+ sum(root.right,num*10+root.val);
+        return pathSum(root.left,num)+ pathSum(root.right,num);
         
     }
     public int sumNumbers(TreeNode root) {
-        return sum(root,0);
+        return pathSum(root,0);
     }
 }
