d9291b1b158832bf69f1d90884fb980e49eac7b2
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-04-06 09:39:38 +0530

    Time: 0 ms (100.00%), Space: 40 MB (53.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,20 @@
 class Solution {
-    public int robberDP(int[] arr,int i,int[] dp){
-        if(i>=arr.length){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int rob=arr[i]+robberDP(arr,i+2,dp);
-        int dontrob=robberDP(arr,i+1,dp);
-        return dp[i]=Math.max(rob,dontrob);
+public int robberBU(int[] arr){
+    if(arr.length==1){
+        return arr[0];
     }
+    int[] dp=new int[arr.length];
+    dp[0]=arr[0];
+    dp[1]=Math.max(arr[0],arr[1]);
+    
+    for(int i=2;i<dp.length;i++){
+        int rob=arr[i]+dp[i-2];
+        int Dont_rob=dp[i-1];
+        dp[i]=Math.max(rob,Dont_rob);
+    }
+    return dp[arr.length-1];
+}
     public int rob(int[] nums) {
-        int[] dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return robberDP(nums,0,dp);
+        return robberBU(nums);
     }
 }
