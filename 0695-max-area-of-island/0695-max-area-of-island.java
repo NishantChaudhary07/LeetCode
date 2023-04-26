35b08d27ef3bddab52671860eab4027dd8396620
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-04-26 11:21:08 +0530

    Time: 2 ms (100.00%), Space: 43.1 MB (27.88%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,25 +1,16 @@
 class Solution {
-    static int count=0;
     
-    public void findIsland(int[][] grid,int cr,int cc,int tr,int tc){
+    public int findIsland(int[][] grid,int cr,int cc,int tr,int tc){
         if(cr<0 || cr>=tr || cc<0 ||cc>=tc || grid[cr][cc]==0){
-            return;
-        }
-        if(grid[cr][cc]==1){
-            count++;
+            return 0;
         }
         grid[cr][cc]=0;
         
-        // int up=findIsland(grid,cr-1,cc,tr,tc);
-        // int down=findIsland(grid,cr+1,cc,tr,tc);
-        // int left=findIsland(grid,cr,cc-1,tr,tc);
-        // int right=findIsland(grid,cr,cc+1,tr,tc);
-        findIsland(grid,cr-1,cc,tr,tc);
-        findIsland(grid,cr+1,cc,tr,tc);
-        findIsland(grid,cr,cc-1,tr,tc);
-        findIsland(grid,cr,cc+1,tr,tc);
-        // return up+down+left+right;
-        // return findIsland(grid,cr-1,cc,tr,tc)+findIsland(grid,cr+1,cc,tr,tc)+
+        int up=findIsland(grid,cr-1,cc,tr,tc);
+        int down=findIsland(grid,cr+1,cc,tr,tc);
+        int left=findIsland(grid,cr,cc-1,tr,tc);
+        int right=findIsland(grid,cr,cc+1,tr,tc);
+        return up+down+left+right+1;
     }
     public int maxAreaOfIsland(int[][] grid) {
         int max_area=0;
@@ -28,11 +19,7 @@ class Solution {
         for(int i=0;i<tr;i++){
             for(int j=0;j<tc;j++){
                 if(grid[i][j]==1){
-                    // System.out.println(findIsland(grid,i,j,tr,tc));
-                    findIsland(grid,i,j,tr,tc);
-                    max_area=Math.max(max_area,count);
-                    count=0;
-                    // max_area=Math.max(max_area,findIsland(grid,i,j,tr,tc));
+                    max_area=Math.max(max_area,findIsland(grid,i,j,tr,tc));
                 }
             }
         }
