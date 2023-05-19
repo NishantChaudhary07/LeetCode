5113bb0ca6a1e3eac16990373bbb95ef3e889880
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-05-19 23:07:52 +0530

    Time: 1 ms (63.09%), Space: 43.3 MB (6.98%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,18 +1,15 @@
 class Solution {
-    private int minCostTopDownDP(int[] cost,int i,int[] dp){
-        if(i>=cost.length){
-            return 0;
+    private int minCostBottomUpDP(int[] cost,int[] dp){
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+        int n=cost.length;
+        for(int i=2;i<n;i++){
+            dp[i]=Math.min(dp[i-1],dp[i-2])+cost[i];
         }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int oneStep=minCostTopDownDP(cost,i+1,dp);
-        int twoStep=minCostTopDownDP(cost,i+2,dp);
-        return dp[i]=Math.min(oneStep,twoStep)+cost[i];
+        return Math.min(dp[n-2],dp[n-1]);
     }
     public int minCostClimbingStairs(int[] cost) {
         int[] dp=new int[cost.length];
-        Arrays.fill(dp,-1);
-        return Math.min(minCostTopDownDP(cost,0,dp),minCostTopDownDP(cost,1,dp));
+        return minCostBottomUpDP(cost,dp);
     }
 }
