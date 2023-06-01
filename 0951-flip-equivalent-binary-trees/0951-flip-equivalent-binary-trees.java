ee08e1ffc26c7cb1b3829975b0aff7565f5f3604
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-06-01 14:25:41 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (63.91%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
@@ -18,15 +18,12 @@ class Solution {
         if(root1==null && root2==null){
             return true;
         }
-        
         if(root1==null || root2==null){
             return false;
         }
-        
         if(root1.val!=root2.val){
             return false;
         }
-       return flipEquiv(root1.left,root2.left) && flipEquiv(root1.right,root2.right) || flipEquiv(root1.left,root2.right) && flipEquiv(root1.right,root2.left) ;
-        
+        return flipEquiv(root1.right,root2.right) && flipEquiv(root1.left,root2.left) || flipEquiv(root1.left,root2.right) && flipEquiv(root1.right,root2.left);
     }
 }
