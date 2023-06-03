c45ae747257e44ffa0c65001164993a5119ac04e
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-06-03 13:35:27 +0530

    Time: 30 ms (45.04%), Space: 43.1 MB (24.64%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,27 +14,27 @@
  * }
  */
 class Solution {
-    private int countPaths(TreeNode root,int targetSum){
+    public int countPath(TreeNode root,int target){
         if(root==null){
             return 0;
         }
-        int ans=0;
-        if(targetSum-root.val==0){
-            ans++;
+        int count=0;
+        if(target-root.val==0){
+            count++;
         }
-        ans=ans+countPaths(root.left,targetSum-root.val)+countPaths(root.right,targetSum-root.val);
-        return ans;
+        count=count+countPath(root.left,target-root.val)+countPath(root.right,target-root.val);
+        return count;
     }
     public int pathSum(TreeNode root, int targetSum) {
         if(root==null){
             return 0;
         }
-        if(root.val==1000000000){
+        if(root.val>1000000){
             return 0;
         }
-        int root_sum=countPaths(root,targetSum);
+        int rooot=countPath(root,targetSum);
         int left=pathSum(root.left,targetSum);
         int right=pathSum(root.right,targetSum);
-        return root_sum+left+right;
+        return rooot+left+right;
     }
 }
