913bb513fa209341172f1a82c9f5c92c64876a48
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-06-03 16:59:37 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (67.79%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -14,20 +14,19 @@
  * }
  */
 class Solution {
-    private void rightView(TreeNode root,List<Integer> lst,int curr){
+    public List<Integer> rightView(TreeNode root,List<Integer> lst,int curr){
         if(root==null){
-            return;
+            return new ArrayList<>();
         }
-        
         if(curr>lst.size()){
             lst.add(root.val);
         }
         rightView(root.right,lst,curr+1);
         rightView(root.left,lst,curr+1);
+        return lst;
     }
-    
     public List<Integer> rightSideView(TreeNode root) {
-     List<Integer> lst=new ArrayList<>();
+        List<Integer> lst=new ArrayList<>();
         rightView(root,lst,1);
         return lst;
     }
