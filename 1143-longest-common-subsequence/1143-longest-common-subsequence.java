c7f2e11f413d1781faf72ca08aa2a1b59264040a
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-06-06 22:20:16 +0530

    Time: 46 ms (13.18%), Space: 48.6 MB (72.18%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,27 +1,27 @@
 class Solution {
-    public int lcs(String text1, String text2,int i,int j,int[][] dp){
-        if(i==text1.length() || j==text2.length()){
+    public int LCS(String st1,String st2,int i,int j,int[][] dp){
+        if(i>=st1.length() || j>=st2.length()){
             return 0;
         }
-        int count=0;
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        if(text1.charAt(i)==text2.charAt(j)){
-            count=1+lcs(text1,text2,i+1,j+1,dp);
+        int count=0;
+        if(st1.charAt(i)==st2.charAt(j)){
+            count=1+LCS(st1,st2,i+1,j+1,dp);
         }
         else{
-            int ignore_first=lcs(text1,text2,i+1,j,dp);
-            int ignore_sec=lcs(text1,text2,i,j+1,dp);
-            count=Math.max(ignore_first,ignore_sec);
+            int ig_first=LCS(st1,st2,i+1,j,dp);
+            int ig_sec=LCS(st1,st2,i,j+1,dp);
+            count=Math.max(ig_first,ig_sec);
         }
-        return dp[i][j]=count;
-    }
+            return dp[i][j]=count;
+        }
     public int longestCommonSubsequence(String text1, String text2) {
         int[][] dp=new int[text1.length()][text2.length()];
         for(int[] arr:dp){
             Arrays.fill(arr,-1);
         }
-        return lcs(text1,text2,0,0,dp);
+        return LCS(text1,text2,0,0,dp);
     }
 }
