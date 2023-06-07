d57046bc4a3f2e1ffec0ac8b03b51552a28386fe
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-06-07 23:42:05 +0530

    Time: 1 ms (95.32%), Space: 44.3 MB (7.69%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -6,18 +6,19 @@ class Solution {
         if(i==dun.length-1 && j==dun[0].length-1){
             return dun[i][j]>0?1:1-dun[i][j];
         }
-        if(dp[i][j]!=Integer.MIN_VALUE){
+        if(dp[i][j]!=-1){
             return dp[i][j];
         }
         int right=helper(dun,i,j+1,dp);
         int down=helper(dun,i+1,j,dp);
-        int energy=Math.min(right,down)-dun[i][j];
-        return dp[i][j]=energy>0?energy:1;
+        int health=Math.min(right,down)-dun[i][j];
+        return dp[i][j]=health>0?health:1;
     }
     public int calculateMinimumHP(int[][] dungeon) {
         int[][] dp=new int[dungeon.length][dungeon[0].length];
         for(int[] arr:dp){
-            Arrays.fill(arr,Integer.MIN_VALUE);
+            Arrays.fill(arr,-1);
+            
         }
         return helper(dungeon,0,0,dp);
     }
