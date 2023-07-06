42ad54affbf01a4b6690a5ce130735c3c949a877
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-07-06 11:25:58 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (28.42%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,21 +1,21 @@
 class Solution {
-    public static void combination(int k,int n,List<Integer> list,List<List<Integer>> res,int idx){
-        if(list.size()==k && n==0){
-            res.add(new ArrayList<Integer>(list));
+    public static void combination(int k,int currsum,int target,int idx,List<Integer> lst,List<List<Integer>> res){
+        if(lst.size()>k){
             return;
         }
+        if(currsum==target && lst.size()==k){
+            res.add(new ArrayList<>(lst));
+        }
         for(int i=idx;i<=9;i++){
-            if(n>=i){
-                list.add(i);
-                combination(k,n-i,list,res,i+1);
-                list.remove(list.size()-1);
-            }
+            lst.add(i);
+            combination(k,currsum+i,target,i+1,lst,res);
+            lst.remove(lst.size()-1);
         }
     }
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> res=new ArrayList<>();
-        List<Integer> list=new ArrayList<>();
-        combination(k,n,list,res,1);
+        List<Integer> lst=new ArrayList<>();
+        combination(k,0,n,1,lst,res);
         return res;
     }
 }
