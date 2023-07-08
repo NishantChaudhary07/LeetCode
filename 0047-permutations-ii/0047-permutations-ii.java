0ba3a71b812a065c7dd18225708aa2463a5d682f
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-07-09 01:56:11 +0530

    Time: 2 ms (97.52%), Space: 44.1 MB (56.43%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,24 +1,24 @@
 class Solution {
     public static void backtrack(int[] nums,List<Integer> lst,List<List<Integer>> res,boolean[] vis){
         if(lst.size()==nums.length){
-            if(!res.contains(lst)){
-            res.add(new ArrayList<>(lst));
-            }
+            res.add(new ArrayList<>(lst)); 
             return;
         }
         
             for(int i=0;i<nums.length;i++){
-                // if(i>0 && nums[i-1]==nums[i]){
-                //     continue;
-                // }
                 if(vis[i]){
                     continue;
                 }
+                if(i>0 && nums[i-1]==nums[i] && !vis[i-1]){
+                    continue;
+                }
+                
                 vis[i]=true;
                 lst.add(nums[i]);
                 backtrack(nums,lst,res,vis);
-                lst.remove(lst.size()-1);
                 vis[i]=false;
+                lst.remove(lst.size()-1);
+                
             }
         }
 
@@ -26,6 +26,7 @@ class Solution {
         List<List<Integer>> res=new ArrayList<>();
         List<Integer> lst=new ArrayList<>();
         boolean[] vis=new boolean[nums.length];
+        Arrays.sort(nums);
         backtrack(nums,lst,res,vis);
         return res;
     }
