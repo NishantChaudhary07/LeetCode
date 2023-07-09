9a5411c56522ba85b5ce0e7fa284eaa8f095ea56
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-07-09 23:23:23 +0530

    Time: 2 ms (97.61%), Space: 44.1 MB (65.74%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,33 +1,30 @@
 class Solution {
-    public static void backtrack(int[] nums,List<Integer> lst,List<List<Integer>> res,boolean[] vis){
+    public static void permute(int[] nums,List<Integer> lst,List<List<Integer>> res,boolean[] vis){
         if(lst.size()==nums.length){
-            res.add(new ArrayList<>(lst)); 
+            res.add(new ArrayList<>(lst));
             return;
         }
         
-            for(int i=0;i<nums.length;i++){
-                if(vis[i]){
-                    continue;
-                }
-                if(i>0 && nums[i-1]==nums[i] && !vis[i-1]){
-                    continue;
-                }
-                
-                vis[i]=true;
-                lst.add(nums[i]);
-                backtrack(nums,lst,res,vis);
-                vis[i]=false;
-                lst.remove(lst.size()-1);
-                
+        for(int i=0;i<nums.length;i++){
+            if(vis[i]){
+                continue;
             }
+            if(i>0 && nums[i]==nums[i-1] && !vis[i-1]){
+                continue;
+            }
+            lst.add(nums[i]);
+            vis[i]=true;
+            permute(nums,lst,res,vis);
+            vis[i]=false;
+            lst.remove(lst.size()-1);
         }
-
+    }
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
         List<Integer> lst=new ArrayList<>();
+        List<List<Integer>> res=new ArrayList<>();
         boolean[] vis=new boolean[nums.length];
         Arrays.sort(nums);
-        backtrack(nums,lst,res,vis);
+        permute(nums,lst,res,vis);
         return res;
     }
 }
