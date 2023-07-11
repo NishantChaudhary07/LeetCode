6432cd262610217e5d5dfe08e4601f5097fbb4c2
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-07-11 11:06:26 +0530

    Time: 196 ms (72.08%), Space: 40.5 MB (90.43%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,32 +1,27 @@
 class Solution {
-    public static boolean search(char[][] board,int cr,int cc,String word,int idx){
+    public static boolean isPresent(char[][] board,String word,int cr,int cc,int idx){
         if(idx==word.length()){
             return true;
         }
-        if(cr<0 || cr>=board.length || cc<0 || cc>=board[0].length || word.charAt(idx)!=board[cr][cc]){
+        if(cc<0 || cr<0 || cc>=board[0].length || cr>=board.length || board[cr][cc]!=word.charAt(idx)){
             return false;
         }
-        
-        int[] r={-1,0,1,0};
-        int[] c={0,-1,0,1};
-        board[cr][cc]='*';
-        for(int i=0;i<r.length;i++){
-            boolean ans=search(board,cr+r[i],cc+c[i],word,idx+1);
-            if(ans)
-                return ans;
-        }
+        board[cr][cc]='#';
+        boolean down=isPresent(board,word,cr+1,cc,idx+1);
+        boolean right=isPresent(board,word,cr,cc+1,idx+1);
+        boolean up=isPresent(board,word,cr-1,cc,idx+1);
+        boolean left=isPresent(board,word,cr,cc-1,idx+1);
         board[cr][cc]=word.charAt(idx);
-        return false;
+        return left||right||up||down;
     }
     
     public boolean exist(char[][] board, String word) {
         for(int i=0;i<board.length;i++){
             for(int j=0;j<board[0].length;j++){
                 if(board[i][j]==word.charAt(0)){
-                    boolean ans=search(board,i,j,word,0);
-                    if(ans){
-                        return ans;
-                    }
+                if(isPresent(board,word,i,j,0)){
+                    return true;
+                }
                 }
             }
         }
