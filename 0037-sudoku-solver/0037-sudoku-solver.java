c10c4f22ef8324e3aac8532cc1acb28aebe8f36c
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-07-12 00:08:05 +0530

    Time: 6 ms (93.66%), Space: 40.2 MB (55.18%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,49 +1,52 @@
 class Solution {
-    public static boolean solver(char[][] board,int row,int col){
-        if(col==board.length){
-            row++;
-            col=0;
+    public static boolean isPossible(char[][] board,int cr,int cc,char val){
+        for(int i=0;i<board.length;i++){
+            if(board[cr][i]==val){
+                return false;
+            }
+            if(board[i][cc]==val){
+                return false;
+            }
         }
-        if(row==board.length){
-            return true;
-        }
-        if(board[row][col]!='.'){
-            return solver(board,row,col+1) ;
-        }
-        else{
-            for(char val='1';val<='9';val++){
-                if(isPossible(board,row,col,val)){
-                    board[row][col]=val;
-                    if(solver(board,row,col+1)) return true;
-                    board[row][col]='.';
+        
+        int row=cr-cr%3;
+        int col=cc-cc%3;
+        for(int i=row;i<row+3;i++){
+            for(int j=col;j<col+3;j++){
+                if(board[i][j]==val){
+                    return false;
                 }
             }
         }
+        return true;
+    }
+    
+    public static boolean solve(char[][] board,int cr,int cc){
+        if(cc==board[0].length){
+            cr++;
+            cc=0;
+        }
+        if(cr==board.length){
+            return true;
+        }
+        if(board[cr][cc]!='.'){
+            return solve(board,cr,cc+1);
+        }
+    
+        for(char i='1';i<='9';i++){
+            if(isPossible(board,cr,cc,i)){
+                board[cr][cc]=i;
+                if(solve(board,cr,cc+1)){
+                    return true;
+                }
+                board[cr][cc]='.';
+            }
+        }
+        
         return false;
     }
-
-    public static boolean isPossible(char[][] board,int row,int col,char val) {
-      for(int i=0;i<board.length;i++){
-          if(board[row][i]==val){
-              return false;
-          }
-          if(board[i][col]==val){
-              return false;
-          }
-      }
-      int r=row-row%3;
-      int c=col-col%3;
-      for(int i=r;i<r+3;i++){
-         for(int j=c;j<c+3;j++){
-             if(board[i][j]==val){
-                 return false;
-             }
-         }
-      }
-      return true;
-    }
-  
+    
     public void solveSudoku(char[][] board) {
-        solver(board,0,0);
+        solve(board,0,0);
     }
 }
