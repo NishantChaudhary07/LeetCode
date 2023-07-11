86d0d6653d2a9b00128932432c6f8d00ea0df827
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-07-11 12:39:57 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (46.68%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -3,13 +3,10 @@ class Solution {
         if(n==0){
             return 1;
         }
-        
         if(n==1){
             return 10;
         }
-        
-        int prod=9;
-        int res=10;
+        int res=10,prod=9;
         for(int i=2;i<=n;i++){
             prod=prod*(11-i);
             res+=prod;
