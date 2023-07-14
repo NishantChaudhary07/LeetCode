fa6378d1790d857102c0d474f7b80f499e2be2a4
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-07-15 02:04:47 +0530

    Time: 140 ms (72.91%), Space: 59.6 MB (12.19%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -3,29 +3,30 @@ class Solution {
         Stack<Integer> st=new Stack<>();
         int max_area=0;
         for(int i=0;i<heights.length;i++){
+            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
+                int ht=heights[st.pop()];
+                if(!st.isEmpty()){
+                    max_area=Math.max(max_area,ht*(i-st.peek()-1));
+                }
+                else{
+                    max_area=Math.max(max_area,ht*i);
+                }
+            }
             
-            while(!st.isEmpty() && heights[i]<=heights[st.peek()]){
-                 int ht=heights[st.pop()];
-                 if(!st.isEmpty()){
-               max_area=Math.max(max_area,ht*(i-st.peek()-1));
-            }
-            else{
-                max_area=Math.max(max_area,ht*(i));
-               }
-            }
-           
-                st.push(i);
+            st.push(i);
         }
-        int l=heights.length;
+        
+        int len=heights.length;
         while(!st.isEmpty()){
             int ht=heights[st.pop()];
-             if(!st.isEmpty()){
-               max_area=Math.max(max_area,ht*(l-st.peek()-1));
+            if(!st.isEmpty()){
+                max_area=Math.max(max_area,ht*(len-st.peek()-1));
             }
             else{
-                max_area=Math.max(max_area,ht*l);
-               }
+                max_area=Math.max(max_area,ht*len);
+            }
         }
+        
         return max_area;
     }
 }
