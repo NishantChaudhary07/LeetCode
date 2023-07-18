6070d64a9d897980f0c149b360223ecfdfe4595b
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-07-19 01:22:44 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (38.98%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -24,16 +24,11 @@ class Solution {
                 list2=list2.next;
             }
         }
-        while(list1!=null){
-                merge.next=new ListNode(list1.val);
-                merge=merge.next;
-                list1=list1.next; 
-        }
-         while(list2!=null){
-                merge.next=new ListNode(list2.val);
-                merge=merge.next;
-                list2=list2.next; 
-        }
+        if(list1==null)
+           merge.next=list2;
+        else
+            merge.next=list1;
+        
         return newhead.next;
     }
 }
