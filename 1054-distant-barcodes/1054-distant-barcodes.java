07111ccd307e87760c98a1fa8983fc47a6746a7b
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-08-04 01:02:28 +0530

    Time: 37 ms (49.15%), Space: 45.4 MB (70.62%) - LeetHub

--- 1054-distant-barcodes/1054-distant-barcodes.java
+++ 1054-distant-barcodes/1054-distant-barcodes.java
@@ -1,20 +1,28 @@
 class Solution {
-      public int[] rearrangeBarcodes(int[] barcodes) {
-        Map<Integer, Integer> m = new HashMap<>();  
-        for (int c : barcodes) { m.put(c, 1 + m.getOrDefault(c, 0)); }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((i, j) -> m.get(j) - m.get(i));
-        pq.addAll(m.keySet());
-        int[] ans = new int[barcodes.length];
-        int index = 0;
-        while (!pq.isEmpty()) {
-            int code = pq.poll(), freq = m.remove(code);
-            while (freq-- > 0) { 
-                if (index >= barcodes.length) { index = 1; } 
-                ans[index] = code;
-                index += 2;
-            } 
-        }   
+    public int[] rearrangeBarcodes(int[] barcodes) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int ele:barcodes){
+            map.put(ele,map.getOrDefault(ele,0)+1);
+        }
+        
+        PriorityQueue<Integer> pq=new PriorityQueue<>((a,b)->map.get(b)-map.get(a));
+        for(int key:map.keySet()){
+            pq.offer(key);
+        }
+        
+        int[] ans=new int[barcodes.length];
+        int idx=0;
+        while(!pq.isEmpty()){
+            int ele=pq.poll();
+            int freq=map.remove(ele);
+            while(freq-->0){
+                ans[idx]=ele;
+                idx+=2;
+                if(idx>=barcodes.length){
+                    idx=1;
+                }
+            }
+        }
         return ans;
-    
     }
 }
