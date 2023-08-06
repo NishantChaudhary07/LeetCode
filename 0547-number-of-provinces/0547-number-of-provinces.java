fa25c37ee85b2e2a17ec680f1f36483d4311afb4
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-08-06 20:17:55 +0530

    Time: 6 ms (11.80%), Space: 45.6 MB (77.06%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int BFS(List<List<Integer>> graph){
+        int count=0;
+        Queue<Integer> q=new LinkedList<>();
+        HashSet<Integer> vis=new HashSet<>();
+        for(int src=0;src<graph.size();src++){
+            if(vis.contains(src)){
+                continue;
+            }
+            count++;
+            q.add(src);
+            while(!q.isEmpty()){
+                int rem=q.poll();
+                if(vis.contains(rem)){
+                    continue;
+                }
+                vis.add(rem);
+                for(int nbr:graph.get(rem)){
+                    if(!vis.contains(nbr)){
+                        q.add(nbr);
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int n=isConnected.length;
+        List<List<Integer>> graph=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            graph.add(i,new ArrayList<>());
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    graph.get(i).add(j);
+                    // graph.get(j+1).add(i+1);
+                }
+            }
+        }
+        return BFS(graph);
+    }
+}
