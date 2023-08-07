33200441777e13ba72dc6c73c4d187e17a43cdf7
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-08-07 23:49:10 +0530

    Time: 7 ms (68.52%), Space: 43.3 MB (85.23%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -1,41 +1,51 @@
 class Solution {
-     public int[] findCordinates(int curr,int n){
-        int[] pos=new int[2];
+    public int[] findCord(int curr,int n){
+        int[] cord=new int[2];
         int r=n-((curr-1)/n)-1;
-        int col=(curr-1)%n;
-        pos[1]=col;
-        pos[0]=r;
+        int c=(curr-1)%n;
+        cord[0]=r;
+        cord[1]=c;
         if(r%2==n%2){
-            pos[1]=n-col-1;
+            cord[1]=n-c-1;
         }
-        return pos;
+        
+        return cord;
     }
-    
     public int snakesAndLadders(int[][] board) {
+        int steps=0;
         int n=board.length;
-        Map<Integer,Integer> map=new HashMap<>();
-        map.put(1,0);
         Queue<Integer> q=new LinkedList<>();
+        boolean[][] vis=new boolean[n][n];
         q.add(1);
+        vis[n-1][0]=true;
         while(!q.isEmpty()){
-            int v=q.remove();
-            if(v==n*n){
-                return map.get(v);
-            }
-            for(int i=1;i<=6;i++){
-                if(v+i>n*n){
-                    break;
+            int size=q.size();
+            while(size-->0){
+                int v=q.poll();
+                if(v==n*n){
+                    return steps;
                 }
-                int[] pos=findCordinates(v+i,n);
-                int r=pos[0];
-                int c=pos[1];
-                int next=board[r][c]==-1?v+i:board[r][c];
-                
-                if(!map.containsKey(next)){
-                    map.put(next,map.get(v)+1);
-                    q.add(next);
+                for(int i=1;i<=6;i++){
+                    if(v+i>n*n){
+                        break;
+                    }
+                    int[] cord=new int[2];
+                    cord=findCord(v+i,n);
+                    int r=cord[0];
+                    int c=cord[1];
+                    if(vis[r][c]==true){
+                        continue;
+                    }
+                    vis[r][c]=true;
+                    if(board[r][c]==-1){
+                        q.add(v+i);
+                    }
+                    else{
+                        q.add(board[r][c]);
+                    }
                 }
             }
+            steps++;
         }
         return -1;
     }
