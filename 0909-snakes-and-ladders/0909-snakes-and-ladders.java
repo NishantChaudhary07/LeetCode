c8326fe22512fdd01447a3bd551a73a736a99eb1
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-08-08 01:11:44 +0530

    Time: 11 ms (34.33%), Space: 43.3 MB (89.25%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.java
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.java
@@ -1,50 +1,48 @@
 class Solution {
     public int[] findCord(int curr,int n){
         int[] cord=new int[2];
-        int r=n-((curr-1)/n)-1;
+        int r=n-(curr-1)/n-1;
         int c=(curr-1)%n;
         cord[0]=r;
         cord[1]=c;
         if(r%2==n%2){
             cord[1]=n-c-1;
         }
-        
         return cord;
     }
     public int snakesAndLadders(int[][] board) {
-        int steps=0;
         int n=board.length;
+        int steps=0;
         Queue<Integer> q=new LinkedList<>();
-        boolean[][] vis=new boolean[n][n];
+        HashSet<Integer> vis=new HashSet<>();
         q.add(1);
-        vis[n-1][0]=true;
+        vis.add(1);
         while(!q.isEmpty()){
             int size=q.size();
             while(size-->0){
-                int v=q.poll();
-                if(v==n*n){
-                    return steps;
+            int v=q.poll();
+            if(v==n*n){
+                return steps;
+            }
+            for(int i=v+1;i<=v+6;i++){
+                if(i>n*n){
+                    break;
                 }
-                for(int i=1;i<=6;i++){
-                    if(v+i>n*n){
-                        break;
-                    }
-                    int[] cord=new int[2];
-                    cord=findCord(v+i,n);
-                    int r=cord[0];
-                    int c=cord[1];
-                    if(vis[r][c]==true){
-                        continue;
-                    }
-                    vis[r][c]=true;
-                    if(board[r][c]==-1){
-                        q.add(v+i);
-                    }
-                    else{
-                        q.add(board[r][c]);
-                    }
+                int[] cord=findCord(i,n);
+                int r=cord[0];
+                int c=cord[1];
+                if(vis.contains(i)){
+                    continue;
+                }
+                vis.add(i);
+                if(board[r][c]==-1){
+                    q.add(i);
+                }
+                else{
+                    q.add(board[r][c]);
                 }
             }
+        }
             steps++;
         }
         return -1;
