95096de4c53ba6bfc213312aac8a9ab2cba3d4a8
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-08-08 02:22:40 +0530

    Time: 1 ms (100.00%), Space: 43 MB (96.26%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,28 +1,32 @@
 class Solution {
-    
-    public int findIsland(int[][] grid,int cr,int cc,int tr,int tc){
-        if(cr<0 || cr>=tr || cc<0 ||cc>=tc || grid[cr][cc]==0){
+    int area=0;
+    public int markIsland(int[][] grid,int cr,int cc,int tr,int tc){
+        if(cr<0 || cc<0 || cr>=tr || cc>=tc || grid[cr][cc]==0){
             return 0;
         }
         grid[cr][cc]=0;
+        area=area+1;
         
-        int up=findIsland(grid,cr-1,cc,tr,tc);
-        int down=findIsland(grid,cr+1,cc,tr,tc);
-        int left=findIsland(grid,cr,cc-1,tr,tc);
-        int right=findIsland(grid,cr,cc+1,tr,tc);
-        return up+down+left+right+1;
+        markIsland(grid,cr-1,cc,tr,tc);
+        markIsland(grid,cr+1,cc,tr,tc);
+        markIsland(grid,cr,cc-1,tr,tc);
+        markIsland(grid,cr,cc+1,tr,tc);
+        return area;
     }
     public int maxAreaOfIsland(int[][] grid) {
-        int max_area=0;
         int tr=grid.length;
         int tc=grid[0].length;
+        int maxArea=0;
+       
         for(int i=0;i<tr;i++){
             for(int j=0;j<tc;j++){
                 if(grid[i][j]==1){
-                    max_area=Math.max(max_area,findIsland(grid,i,j,tr,tc));
+                    area=0;
+                    maxArea=Math.max(maxArea,markIsland(grid,i,j,tr,tc));
+                
                 }
             }
         }
-        return max_area;
+        return maxArea;
     }
 }
