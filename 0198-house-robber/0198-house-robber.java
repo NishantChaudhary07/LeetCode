a5634dd3b29e709dda382ca274868d15899f29ed
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-08-22 18:22:41 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (40.00%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,19 @@
 class Solution {
-    public int maxAmt(int[] nums,int idx,int[] dp){
+    public int maxRob(int[] nums,int idx,int[] dp){
         if(idx>=nums.length){
             return 0;
         }
         if(dp[idx]!=-1){
             return dp[idx];
         }
-        int rob=nums[idx]+maxAmt(nums,idx+2,dp);
-        int dontRob=maxAmt(nums,idx+1,dp);
+        int rob=nums[idx]+maxRob(nums,idx+2,dp);
+        int dontRob=maxRob(nums,idx+1,dp);
         return dp[idx]=Math.max(rob,dontRob);
+        
     }
     public int rob(int[] nums) {
         int[] dp=new int[nums.length];
         Arrays.fill(dp,-1);
-        return maxAmt(nums,0,dp);
+        return maxRob(nums,0,dp);
     }
 }
