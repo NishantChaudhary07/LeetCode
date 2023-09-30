fb2bb36847a316097083dab8d3a569214dd25329
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-09-30 16:34:28 +0530

    Added solution - LeetHub

---
+++ Boolean Matrix - GFG/boolean-matrix.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split("\\s+");
+            int r = Integer.parseInt(str[0]);
+            int c = Integer.parseInt(str[1]);
+            int matrix[][] = new int[r][c];
+            
+            for(int i = 0; i < r; i++)
+            {
+                int k = 0;
+                str = read.readLine().trim().split("\\s+");
+                for(int j = 0; j < c; j++){
+                  matrix[i][j] = Integer.parseInt(str[k]);
+                  k++;
+                }
+            }
+            new Solution().booleanMatrix(matrix);
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < r; i++){
+                for(int j = 0; j < c; j++){
+                    sb.append(matrix[i][j] + " ");
+                }
+                sb.append("\n"); 
+            }
+            System.out.print(sb);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to modify the matrix such that if a matrix cell matrix[i][j]
+    //is 1 then all the cells in its ith row and jth column will become 1.
+    void booleanMatrix(int matrix[][])
+    {
+        // code here 
+        Set<Integer> row=new HashSet<>();
+        Set<Integer> col=new HashSet<>();
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==1){
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+        for(int r:row){
+            for(int j=0;j<matrix[0].length;j++){
+                matrix[r][j]=1;
+            }
+        }
+        for(int c:col){
+            for(int j=0;j<matrix.length;j++){
+                matrix[j][c]=1;
+            }
+        }
+    }
+}
