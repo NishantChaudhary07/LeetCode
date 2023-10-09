f18625e5c666ef4bf6bb1b4c78b1713871a034f1
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-10-09 15:52:43 +0530

    Time: 1 ms (99.44%), Space: 44.5 MB (26.50%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,28 +1,30 @@
 class Solution {
-    private int helper(int[][] mat,int i,int j,int[][] dp){
-        if(j==mat[0].length || j<0){
+    int helper(int[][] grid,int r,int c,int[][] dp){
+        if(r==grid.length || c<0 || c==grid.length){
             return Integer.MAX_VALUE;
         }
-        if(i==mat.length-1){
-            return mat[i][j];
+        if(r==grid.length-1){
+            return grid[r][c];
         }
-        if(dp[i][j]!=Integer.MIN_VALUE){
-            return dp[i][j];
+        
+        if(dp[r][c]!=Integer.MIN_VALUE){
+            return dp[r][c];
         }
-        int left=helper(mat,i+1,j-1,dp);
-        int down=helper(mat,i+1,j,dp);
-        int right=helper(mat,i+1,j+1,dp);
-        return dp[i][j]=mat[i][j]+Math.min(left,Math.min(right,down));
+        
+        int dia_left=helper(grid,r+1,c-1,dp);
+        int down=helper(grid,r+1,c,dp);
+        int dia_right=helper(grid,r+1,c+1,dp);
+        return dp[r][c]=Math.min(dia_left,Math.min(down,dia_right))+grid[r][c];
     }
     public int minFallingPathSum(int[][] matrix) {
         int[][] dp=new int[matrix.length][matrix[0].length];
         for(int[] arr:dp){
-                Arrays.fill(arr,Integer.MIN_VALUE);
-            }
-        int res=Integer.MAX_VALUE;
-        for(int i=0;i<matrix[0].length;i++){
-            res=Math.min(res,helper(matrix,0,i,dp));
+            Arrays.fill(arr,Integer.MIN_VALUE);
         }
-        return res;
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<matrix[0].length;i++){
+            ans=Math.min(ans,helper(matrix,0,i,dp));
+        }
+        return ans;
     }
 }
