abc5ee3ecb4cb056ed169422ee50cafa999ffb6d
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2023-10-09 12:59:10 +0530

    Time: 1 ms (21.02%), Space: 44.2 MB (70.16%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,47 +1,16 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] res=new int[2];
-        int start=0,end=nums.length-1;
-        res[0]=-1;
-        res[1]=-1;
-        if(nums.length==0){
-            return res;
-        }
-        int pos=-1;
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(nums[mid]==target){
-                pos=mid;
-                break;
-            }
-            else if(target>nums[mid]){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
+        int[] res={-1,-1};
+        boolean first=false;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                if(first==false){
+                    res[0]=i;
+                    first=true;
+                }
+                res[1]=i;
             }
         }
-        System.out.println(pos);
-        int initial=pos;
-        int finalpos=pos;
-        if(pos!=-1){
-        while(initial>1 && nums[initial-1]==nums[initial]){
-            --initial;
-        }
-        if(initial>0 && nums[initial-1]==nums[initial]){
-            initial--;
-       }
-        while(finalpos<nums.length-1 && nums[finalpos+1]==nums[finalpos]){
-            ++finalpos;
-        }
-            if(finalpos==nums.length-2 && nums[finalpos+1]==nums[finalpos]){
-                finalpos++;
-            }
-             res[0]=initial;
-           res[1]=finalpos;
-        }
-       
-        
         return res;
     }
 }
