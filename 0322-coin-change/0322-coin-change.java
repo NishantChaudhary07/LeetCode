7e4be9875128312e11d3b00567d728e65c754468
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2024-06-05 17:08:46 +0530

    Time: 28 ms (26.62%), Space: 44.3 MB (59.83%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,28 +1,26 @@
 class Solution {
-    public int coinChang(int[] coins,int amt,int[] dp){
-        if(amt<0){
-            return Integer.MAX_VALUE;
-        }
+    public int helper(int[] coins,int amt,int[] dp){
         if(amt==0){
             return 0;
         }
         if(dp[amt]!=-1){
             return dp[amt];
         }
-        int finalcount=Integer.MAX_VALUE;
+        int updcount=Integer.MAX_VALUE;
         for(int i=0;i<coins.length;i++){
-            int currcount=coinChang(coins,amt-coins[i],dp);
-            if(currcount!=Integer.MAX_VALUE && currcount+1<finalcount){
-                finalcount=currcount+1;
+            if(amt-coins[i]>=0){
+                int curr=helper(coins,amt-coins[i],dp);
+                if(curr!=Integer.MAX_VALUE && curr+1<updcount){
+                    updcount=curr+1;
+                }
             }
         }
-        return dp[amt]=finalcount;
+        return dp[amt]=updcount;
     }
     public int coinChange(int[] coins, int amount) {
         int[] dp=new int[amount+1];
         Arrays.fill(dp,-1);
-        int res=coinChang(coins,amount,dp);
+        int res=helper(coins,amount,dp);
         return res==Integer.MAX_VALUE?-1:res;
-    
     }
 }
