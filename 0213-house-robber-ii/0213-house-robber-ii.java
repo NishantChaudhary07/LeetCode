4bfe2fabe808976692ffc4dbc30729bea61a0000
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2024-06-09 20:44:48 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (69.90%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,26 +1,25 @@
 class Solution {
-    public int maxRob(int[] nums,int idx,int n,int[] dp){
+    public int helper(int[] nums,int idx,int n,int[] dp){
         if(idx>=n){
             return 0;
         }
         if(dp[idx]!=-1){
             return dp[idx];
         }
-        int rob=nums[idx]+maxRob(nums,idx+2,n,dp);
-        int dontRob=maxRob(nums,idx+1,n,dp);
-        return dp[idx]=Math.max(rob,dontRob);
+        int take=nums[idx]+helper(nums,idx+2,n,dp);
+        int dont_take=helper(nums,idx+1,n,dp);
+        return dp[idx]=Math.max(take,dont_take);
     }
     public int rob(int[] nums) {
         int n=nums.length;
         if(n==1){
             return nums[0];
         }
-        int[] dp=new int[nums.length];
+        int[] dp=new int[n];
         Arrays.fill(dp,-1);
-        int first=maxRob(nums,0,n-1,dp);
+        int first=helper(nums,0,n-1,dp);
         Arrays.fill(dp,-1);
-        int sec=maxRob(nums,1,n,dp);
+        int sec=helper(nums,1,n,dp);
         return Math.max(first,sec);
-        
     }
 }
