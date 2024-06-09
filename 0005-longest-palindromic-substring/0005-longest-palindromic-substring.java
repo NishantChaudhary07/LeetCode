3f6eb5a14891eb3c33367ce24fa64bc536acd974
Nishant Chaudhary <74598012+NishantChaudhary07@users.noreply.github.com>
2024-06-09 10:26:30 +0530

    Time: 222 ms (27.14%), Space: 45.5 MB (24.99%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,39 +1,36 @@
 class Solution {
-    public static String expand(String s,int start,int end,String res){
-         
-           while(start>=0 && end<s.length()){
-               if(s.charAt(start)!=s.charAt(end))
-               {
-                  break; 
-               }
-               if(end!=start)
-                   res=s.charAt(start)+res+s.charAt(end);
-               else if(end==start)
-                   res=res+s.charAt(end);
-            
-                   start--;
-                   end++;
-           }
-        return res;
-    }
-    public String longestPalindrome(String s) {
-        int mxlen=0;
-        String ans="";
-        if(s.length()<=1){
-            return s;
+    public String extend(String s,int left,int right,String res){
+            while(left>=0 && right<s.length()){
+                if(s.charAt(left)!=s.charAt(right)){
+                    break;
+                }
+                if(left!=right){
+                    res=s.charAt(left)+res+s.charAt(right);
+                }
+                else if(left==right){
+                    res+=s.charAt(right);
+                }
+                left--;
+                right++;
+            }
+            return res;
         }
-       for(int i=0;i<s.length();i++){
-          String res=expand(s,i,i,"");
-           String res1=expand(s,i,i+1,"");
-           if(res.length()>mxlen){
-               mxlen=res.length();
-               ans=res;
-           }
-           if(res1.length()>mxlen){
-               mxlen=res1.length();
-               ans=res1;
-           }
-       }
+    public String longestPalindrome(String s) {
+        String ans="";
+        int mxlen=0;
+        for(int i=0;i<s.length();i++){
+            String res=extend(s,i,i,"");
+            String res1=extend(s,i,i+1,"");
+            if(res.length()>mxlen){
+                mxlen=res.length();
+                ans=res;
+            }
+            if(res1.length()>mxlen){
+                mxlen=res1.length();
+                ans=res1;
+            }
+            
+        }
         return ans;
     }
 }
